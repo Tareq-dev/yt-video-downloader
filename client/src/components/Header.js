b053ea30220ccffaa213672fcbd5c4ee8342f42b
Tareq-dev <tareque.dev@gmail.com>
2023-06-06 22:15:54 +0600

    design header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import Result from "./Result";
+import axios from "axios";
+
+export default function () {
+  const [videoUrl, setVideoUrl] = useState("");
+  const [data, setData] = useState({});
+  const [result, setResult] = useState(false);
+  const [error, setError] = useState("");
+  // const url = "http://localhost:5000";
+
+  const handleSubmit = async () => {
+    if (videoUrl) {
+      setVideoUrl("");
+      try {
+        const response = await fetch("http://localhost:5000/api/download", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ videoUrl }),
+        });
+        const data = await response.json();
+
+        setData(data.data);
+        setResult(true);
+      } catch (error) {
+        setError(error);
+      }
+    } else {
+      setError("Invalid Url, Please paste proper Url");
+    }
+  };
+  return (
+    <div>
+      <h1 className="text-3xl md:text-4xl text-center text-white font-extrabold pt-14">
+        Youtube to mp3 Downloader
+      </h1>
+      <p className="text-lg md:text-xl text-center pt-2 font-bold">
+        Download Youtube Video . Convert to mp4 Format
+      </p>
+      <div className="flex justify-center pt-10">
+        <div className="py-10 px-4 mx-2 md:px-12 rounded-md md:w-2/3 bg-black">
+          <div className="flex justify-center">
+            <label
+              htmlFor=""
+              className="md:text-xl pb-4 text-white text-center"
+            >
+              Paste video url address and press Go to start
+            </label>
+          </div>
+          <div className="flex">
+            <input
+              className="w-full px-4 py-2 rounded-l-md outline-none"
+              type="text"
+              name=""
+              placeholder="Enter link here ..."
+              onChange={(e) => setVideoUrl(e.target.value)}
+            />
+            <button
+              onClick={handleSubmit}
+              className="px-4 md:px-10 py-2 rounded-r-md font-bold text-2xl bg-red-500 text-white"
+              type=""
+            >
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
+      {result ? (
+        <Result videoUrl={videoUrl} data={data} />
+      ) : (
+        <p className="text-center font-bold text-red-500 pt-8 text-xl px-4 ">
+          {error}
+        </p>
+      )}
+    </div>
+  );
+}
